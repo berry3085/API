3301c15995025faf6bb03ff3866df11f04f552f4
berry <berry3085@kimo.com>
2022-01-29 00:26:11 +0800

    Initial Commit

---
+++ Shared/APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIApp.swift
+//  Shared
+//
+//  Created by 沈秉叡 on 2022/1/29.
+//
+
+import SwiftUI
+
+@main
+struct APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
